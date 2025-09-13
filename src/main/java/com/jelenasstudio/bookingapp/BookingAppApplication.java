83b7a4870e66fa36f2ecb236b00bc009d96fcfb7
John Doe <arina.soldan@gmail.com>
2025-09-13 21:50:47 +0300

    Add admin for app

--- src/main/java/com/jelenasstudio/bookingapp/BookingAppApplication.java
+++ src/main/java/com/jelenasstudio/bookingapp/BookingAppApplication.java
@@ -1,13 +1,48 @@
 package com.jelenasstudio.bookingapp;
 
+import com.jelenasstudio.bookingapp.model.Role;
+import com.jelenasstudio.bookingapp.model.User;
+import com.jelenasstudio.bookingapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Set;
+
+@RequiredArgsConstructor
 @SpringBootApplication
 public class BookingAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingAppApplication.class, args);
-	}
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initAdmin() {
+        return args -> {
+            if (!userRepository.existsByEmail(adminEmail)) {
+                User admin = User.builder()
+                        .name("Admin")
+                        .email(adminEmail)
+                        .password(passwordEncoder.encode(adminPassword))
+                        .roles(Set.of(Role.ROLE_ADMIN))
+                        .build();
+                userRepository.save(admin);
+            }
+        };
+    }
 
 }
